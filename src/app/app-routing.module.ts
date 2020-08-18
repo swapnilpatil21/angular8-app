99e4f2b9f89cc68d0be06ebfa820827b392d6655
swapnilpatil21 <swapnil.a.patil@gslab.com>
2020-08-18 14:29:39 +0530

    Generated product-get component and added routes and menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductGetComponent } from './product-get/product-get.component';  
 
-const routes: Routes = [];
+const routes: Routes = [
+  {  
+    path: 'products',  
+    component: ProductGetComponent  
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductGetComponent } from './product-get/product-get.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductGetComponent
   ],
   imports: [
     BrowserModule,
